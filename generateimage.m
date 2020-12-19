4feb02c8f4d3b74b51b89507bd2c489f127fd553
Pak Ming Wan <pakmingw@gmail.com>
2020-12-19 23:00:47 +0100

    updated color shift

--- generateimage.m
+++ generateimage.m
@@ -47,9 +47,9 @@ f3 = f3';
 zeroMatrix = zeros(size(f));
 
 # color shifter, if you want the colors to move slightly
-rshift = uint8(floor(a/10 * sin((x - f1offset) * 2 * pi / max) + (a / 2)));
-gshift = uint8(floor(a/10 * sin((x - f2offset) * 2 * pi / max) + (a / 2)));
-bshift = uint8(floor(a/10 * sin((x - f3offset) * 2 * pi / max) + (a / 2)));
+rshift = uint8(floor(a/10 * sin((x - f1offset) * 2 * pi / max) ));
+gshift = uint8(floor(a/10 * sin((x - f2offset) * 2 * pi / max) ));
+bshift = uint8(floor(a/10 * sin((x - f3offset) * 2 * pi / max) ));
 
 rshift = rshift';
 gshift = gshift';

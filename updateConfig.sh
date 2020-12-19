4675872f078376d2bf2b3f64cb40c7843b8bfd5f
Pak Ming Wan <pakmingw@gmail.com>
2020-12-19 22:24:34 +0100

    added service restart

--- updateConfig.sh
+++ updateConfig.sh
@@ -5,3 +5,6 @@ sudo cp keybow.lua /boot/keybow.lua
 sudo cp layouts/mini_googlemeet.lua /boot/layouts/mini_googlemeet.lua
 sudo cp layouts/mini_googlemeet_png.lua /boot/layouts/mini_googlemeet_png.lua
 sudo cp default.png /boot/default.png
+
+# Restart keybow service
+sudo systemctl restart keybow
